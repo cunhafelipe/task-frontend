28f372e2d141d4899de4fd2cbd178ac6e9be1d57
cunhafelipe <felipe.cunha2001@hotmail.com>
2024-07-10 21:36:48 -0300

    feat: add task item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,9 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import "./index.scss";
-
-import TaskItem from "./TaskItem";
+import Tasks from "./components/Tasks";
 
 function App() {
-  const [task, setTask] = useState([]);
-
-  const fetchTask = async () => {
-    try {
-      const response = await axios.get(
-        "https://task-manager-backend-7y59.onrender.com/tasks"
-      );
-      setTask(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchTask();
-  }, []);
-
   return (
     <div>
-      {task.map((task, index) => (
-        <TaskItem task={task} key={index} />
-      ))}
+      <Tasks />
     </div>
   );
 }
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import "./Tasks.scss";
+
+export default function Tasks() {
+  const [tasks, setTask] = useState([]);
+
+  const unfinishedTasks = tasks.filter((task) => task.isCompleted === false);
+  const finishedTasks = tasks.filter((task) => task.isCompleted === true);
+
+  const fetchTask = async () => {
+    try {
+      const response = await axios.get(
+        "https://task-manager-backend-7y59.onrender.com/tasks"
+      );
+      setTask(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTask();
+  }, []);
+
+  return (
+    <div className="tasks-container">
+      <h2>Últimas tarefas</h2>
+
+      <div className="last-tasks">
+        <h3>Últimas tarefas</h3>
+        <div className="tasks-list">
+          {unfinishedTasks.map((lastTask, index) => (
+            <p key={index}>{lastTask.description}</p>
+          ))}
+        </div>
+      </div>
+
+      <div className="completed-tasks">
+        <h3>Tarefas Concluídas</h3>
+        <div className="tasks-list">
+          {finishedTasks.map((finishedTask, index) => (
+            <p key={index}>{finishedTask.description}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
