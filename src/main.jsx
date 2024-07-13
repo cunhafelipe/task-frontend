94b42a73cd08e3d86ad56a9dfa5bacb5dccd94f7
cunhafelipe <felipe.cunha2001@hotmail.com>
2024-07-13 12:38:41 -0300

    feat: add react-router-dom setup and home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+
 import "./index.scss";
+import App from "./App.jsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <h1>Hello</h1>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>
 );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import {} from "react";
+import Tasks from "../components/Tasks";
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <Tasks />
+    </div>
+  );
+}
